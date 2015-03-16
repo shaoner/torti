f2035f05ff452400910a28ac8a23880c060912cb
Alexandre Laurent <shaoner@gmail.com>
2015-03-16 15:05:36 +0100

    Add Field unit tests covering clone & random unique name

--- tests/field.js
+++ tests/field.js
@@ -36,5 +36,47 @@ module.exports = {
         var r = form.render();
         test.equals(r.fields[0].foo, 'bar');
         test.done();
+    },
+    Field_random_name: function (test) {
+        var f = Field();
+        test.ok(f._name.match('name_') != null);
+        test.done();
+    },
+    Field_clone: function (test) {
+        var f = Field({ name: 'foo', hello: 'world' });
+        var clone = f.clone();
+        clone.setValue('bar');
+        test.equals(f.value(), '');
+        test.equals(clone.value(), 'bar');
+        test.equals(clone._name, 'foo');
+        test.equals(clone._options.hello, 'world');
+        test.done();
+    },
+    Field_clone_with_validators: function (test) {
+        var f = Field({ name: 'foo', hello: 'world' }).isLength(5, 20).isEmail();
+        var clone = f.clone();
+
+        clone.setValue('bar');
+        test.equals(clone.validate(), false);
+        test.equals(clone.errors()[0], 'length');
+
+        clone.setValue('barjo');
+        test.equals(clone.validate(), false);
+        test.equals(clone.errors()[0], 'format');
+
+        clone.setValue('barjo@nomail.com');
+        test.equals(clone.validate(), true);
+        test.equals(clone.errors().length, 0);
+        test.done();
+    },
+    Field_clone_with_errors: function (test) {
+        var f = Field({ name: 'foo', hello: 'world' }).isLength(5, 20).isEmail();
+        var clone = f.clone();
+        f.setValue('bar');
+        f.validate();
+        test.equals(f.errors().length, 1);
+        test.equals(clone.errors().length, 0);
+        test.done();
     }
+
 };
